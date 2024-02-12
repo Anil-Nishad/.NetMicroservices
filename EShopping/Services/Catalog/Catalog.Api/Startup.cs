db2ba725e453a22fc6c17c5bae555b13ecceb9b7
ANIL KUMAR <anilnishad298@gmail.com>
2024-02-12 23:51:48 +0530

    Initial Commit

---
+++ EShopping/Services/Catalog/Catalog.Api/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Catalog.Api;
+
+public class Startup
+{
+    public IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            //endponits.MapHealthChecks(pattern: "/health", new HealthCheckOptions()
+            //{
+            //    Predicate = _ = true,
+            //    ResponseWriter = UI
+            //});
+        });
+    }
+}
